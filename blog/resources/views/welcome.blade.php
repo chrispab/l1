d5b10cbe55d95bed8125d5f4befb5ba30a8a3cbd
chrispab <cbattisson@gmail.com>
2019-08-11 21:26:16 +0100

    8

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #4f4;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
