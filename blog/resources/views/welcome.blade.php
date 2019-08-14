f3d9f011c94328148b05eb6027cfd77106970824
chrispab <cbattisson@gmail.com>
2019-08-14 23:50:47 +0100

    text

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -1,8 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>        <meta charset="utf-8">
+            <script>
+(function() {
+  var app;
+
+  $(document).ready(function() {
+    return app.init();
+  });
+
+  app = {
+    text: "Hello",
+    index: 0,
+    chars: 0,
+    speed: 200,
+    container: ".text .content",
+    init: function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+    write: function() {
+      $(this.container).append(this.text[this.index]);
+      if (this.index < this.chars) {
+        this.index++;
+        return window.setTimeout(function() {
+          return app.write();
+        }, this.speed);
+      }
+    }
+  };
+
+}).call(this);
+                          </script>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>.</title>
 
@@ -61,7 +92,47 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            @import url(https://fonts.googleapis.com/css?family=Roboto);
+body {
+  /* font-family: 'Roboto'; */
+  color: #20b2e3;
+}
+.text {
+  width: 100%;
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  font-size: 30px;
+  /* position: absolute; */
+  top: 0;
+  bottom: 0;
+  margin: auto;
+}
+.text .content {
+  display: inline;
+}
+.text .dash {
+  display: inline-block;
+  height: 3px;
+  width: 20px;
+  background-color: #3498db;
+  animation: blink 0.5s infinite linear;
+}
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  99% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
         </style>
+
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -81,13 +152,21 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                .
+                        <div class="text">
+                                <div class="content">
+
+                                </div>
+                                <div class="dash"></div>
+                              </div>
                 </div>
 
                 <div class="links">
-                    <a href="http://192.168.0.230/">Zones Consoles</a>
+                    <a href="http://192.168.0.230/">Zone Logs</a>
                     <a href="http://192.168.0.200:8080/habpanel/index.html#/view/Temperature">Hab Dash</a>
                     <a href="http://192.168.0.200:8080/habpanel/index.html#/view/Zones">Hab Zones</a>
+
+
+
                 </div>
             </div>
         </div>
