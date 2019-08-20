0decbe4c8218bd3fc65576403b7b3dd890716819
chrispab <cbattisson@gmail.com>
2019-08-20 21:25:30 +0100

    use mix

--- blog/resources/js/hab.js
+++ blog/resources/js/hab.js
@@ -1,29 +1,29 @@
 (function() {
     var app;
-  
+
     $(document).ready(function() {
-      return app.init();
+        return app.init();
     });
-  
+
     app = {
-      text: "Hello",
-      index: 0,
-      chars: 0,
-      speed: 200,
-      container: ".text .content",
-      init: function() {
-        this.chars = this.text.length;
-        return this.write();
-      },
-      write: function() {
-        $(this.container).append(this.text[this.index]);
-        if (this.index < this.chars) {
-          this.index++;
-          return window.setTimeout(function() {
-            return app.write();
-          }, this.speed);
+        text: "_____",
+        index: 0,
+        chars: 0,
+        speed: 200,
+        container: ".text .content",
+        init: function() {
+            this.chars = this.text.length;
+            return this.write();
+        },
+        write: function() {
+            $(this.container).append(this.text[this.index]);
+            if (this.index < this.chars) {
+                this.index++;
+                return window.setTimeout(function() {
+                    return app.write();
+                }, this.speed);
+            }
         }
-      }
     };
-  
-  }).call(this);
+
+}).call(this);
